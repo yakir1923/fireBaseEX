f475ec6d176d7f4aea11669b973b72ff69241964
shanimoz <65355962+shanimoz@users.noreply.github.com>
2020-06-15 20:48:40 +0300

    letters

---
+++ app/src/main/java/com/example/firebaseex/Letter.java
@@ -0,0 +1,43 @@
+package com.example.firebaseex;
+
+import android.media.Image;
+
+public class Letter {
+    private char lett;
+    private int scoring;
+    private Image image;
+
+    public Letter(){
+    }
+
+    public Letter(char lett, int scoring, Image image) {
+        this.lett = lett;
+        this.scoring = scoring;
+        this.image = image;
+    }
+
+    public char getLett() {
+        return lett;
+    }
+
+    public void setLett(char lett) {
+        this.lett = lett;
+    }
+
+    public int getScoring() {
+        return scoring;
+    }
+
+    public void setScoring(int scoring) {
+        this.scoring = scoring;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+}
+
