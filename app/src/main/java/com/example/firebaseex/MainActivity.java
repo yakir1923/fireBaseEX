1e0547c25c3f41ead0a9edce5aa1c8391845e638
shanimoz <65355962+shanimoz@users.noreply.github.com>
2020-07-16 17:14:47 +0300

    letters

--- app/src/main/java/com/example/firebaseex/MainActivity.java
+++ app/src/main/java/com/example/firebaseex/MainActivity.java
@@ -30,89 +30,36 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     // Access a Cloud Firestore instance from your Activity
     FirebaseFirestore db = FirebaseFirestore.getInstance();
-int dell;
-    public static String nameOfCollection="Users";
+
+    public static String nameOfCollection = "Users";
     private EditText name;
-    private  EditText lastName;
+    private Button googleButton;
+    private EditText lastName;
     private EditText email;
     private StringBuilder text = new StringBuilder();
-    public static final int GOOGLE_SIGN_IN=1;
+    public static final int GOOGLE_SIGN_IN = 1;
     FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-    private HashMap<String,String> texts=new HashMap();
+    private HashMap<String, String> texts = new HashMap();
     List<AuthUI.IdpConfig> providers = Arrays.asList(
             new AuthUI.IdpConfig.GoogleBuilder().build()
     );
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.i("CurrentUser", ""+user);
-        Button button=findViewById(R.id.send_to_firebase);
-        startActivityForResult(
-                AuthUI.getInstance()
-                        .createSignInIntentBuilder()
-                        .setAvailableProviders(providers)
-                        .build(),
-                GOOGLE_SIGN_IN);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button button = findViewById(R.id.send_to_firebase);
+        googleButton = findViewById(R.id.google_button);
+        googleButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                 name=findViewById(R.id.name);
-                lastName=findViewById(R.id.last_name);
-                email=findViewById(R.id.email);
-                User user=new User(name.getText().toString(),lastName.getText().toString(),email.getText().toString());
-              //  db.collection(nameOfCollection).document(user.getEmail()).set(user);
-                db.collection(nameOfCollection).document(name.getText().toString()).set(user).addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        Toast.makeText(MainActivity.this,"ok",Toast.LENGTH_LONG).show();
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Toast.makeText(MainActivity.this,"not ok",Toast.LENGTH_LONG).show();
-                    }
-                });
 
+                googleOnClick();
             }
         });
-        BufferedReader reader = null;
 
-        try {
-            reader = new BufferedReader(
-                    new InputStreamReader(getAssets().open("text.txt")));
-
-            // do reading, usually loop until end of file reading
-            String mLine;
-            int i=0;
-            while ((mLine = reader.readLine()) != null) {
-
-                texts.put(mLine.split(":")[0],mLine.split(":")[1]);
-
-
-            }
-            db.collection("translation").document("translation").set(texts);
-            db.collection("translation").document("translation").get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                @Override
-                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                    HashMap<String,String> translation=new HashMap<>();
-                    Log.i("TAG", "onComplete: "+task.getResult().get("test"));
-                }
-            });
-        } catch (IOException e) {
-            Toast.makeText(getApplicationContext(), "Error reading file!", Toast.LENGTH_LONG).show();
-            e.printStackTrace();
-        } finally {
-            if (reader != null) {
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    //log the exception
-                }
-
-            }
-        }
     }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
@@ -131,4 +78,73 @@ int dell;
             }
         }
     }
+
+    public void addLetters() {
+        BufferedReader reader = null;
+
+        try {
+            reader = new BufferedReader(
+                    new InputStreamReader(getAssets().open("text.txt")));
+
+            // do reading, usually loop until end of file reading
+            String mLine;
+            int i = 0;
+            while ((mLine = reader.readLine()) != null) {
+
+                texts.put(mLine.split(":")[0], mLine.split(":")[1]);
+
+
+            }
+            db.collection("translation").document("translation").set(texts);
+            db.collection("translation").document("translation").get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    HashMap<String, String> translation = new HashMap<>();
+                    Log.i("TAG", "onComplete: " + task.getResult().get("test"));
+                }
+            });
+        } catch (IOException e) {
+            Toast.makeText(getApplicationContext(), "Error reading file!", Toast.LENGTH_LONG).show();
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    //log the exception
+                }
+
+            }
+        }
+    }
+
+    public void fireBaseOnClick() {
+        name = findViewById(R.id.name);
+        lastName = findViewById(R.id.last_name);
+        email = findViewById(R.id.email);
+        User user = new User(name.getText().toString(), lastName.getText().toString(), email.getText().toString());
+        //  db.collection(nameOfCollection).document(user.getEmail()).set(user);
+        db.collection(nameOfCollection).document(name.getText().toString()).set(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                Toast.makeText(MainActivity.this, "ok", Toast.LENGTH_LONG).show();
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, "not ok", Toast.LENGTH_LONG).show();
+            }
+        });
+
+    }
+
+    public void googleOnClick() {
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .build(),
+                GOOGLE_SIGN_IN);
+    }
 }
+
