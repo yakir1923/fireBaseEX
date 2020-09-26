cc640027606b5a8c8bf701b098fb485f854b2628
shanimoz <65355962+shanimoz@users.noreply.github.com>
2020-09-26 22:26:21 +0300

    letters

---
+++ app/src/main/java/com/example/firebaseex/boxesPage.java
@@ -0,0 +1,14 @@
+package com.example.firebaseex;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class boxesPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boxes_page);
+    }
+}
