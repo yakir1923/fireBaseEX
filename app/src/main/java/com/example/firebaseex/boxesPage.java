ff8abcba5866c1bcdda8a20f256071525d0d3e32
shanimoz <65355962+shanimoz@users.noreply.github.com>
2020-10-01 00:16:50 +0300

    letters

--- app/src/main/java/com/example/firebaseex/boxesPage.java
+++ app/src/main/java/com/example/firebaseex/boxesPage.java
@@ -10,5 +10,7 @@ public class boxesPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_boxes_page);
+
+        //דף תיבות
     }
 }
