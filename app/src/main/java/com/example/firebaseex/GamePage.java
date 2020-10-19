1835cf1d1ebb918cc883cdd64c051997fa5b3847
yakir <yakir1923@gmail.com>
2020-10-19 20:05:44 +0300

    19/10/20 100 כפתורים אחד על השני

--- app/src/main/java/com/example/firebaseex/GamePage.java
+++ app/src/main/java/com/example/firebaseex/GamePage.java
@@ -1,16 +1,33 @@
 package com.example.firebaseex;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.RelativeLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class GamePage extends AppCompatActivity {
-
-
+private TableRow tableRow;
+private Button button;
+private    int i,j;
+private RelativeLayout relativeLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_page);
+        relativeLayout=findViewById(R.id.game_layout1);
+
+        for (i=0;i<100;i++) {
+            relativeLayout.addView(new Button(this));
+        }
 
 
     }
