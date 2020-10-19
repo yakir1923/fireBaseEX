9a28d7a812746b3c56504530138a537d296f7b6e
shanimoz <65355962+shanimoz@users.noreply.github.com>
2020-10-19 23:03:55 +0300

    רציתי להוסיף כפתורים ושיניתי צבעים

--- app/src/main/java/com/example/firebaseex/GamePage.java
+++ app/src/main/java/com/example/firebaseex/GamePage.java
@@ -2,17 +2,10 @@ package com.example.firebaseex;
 
 import android.os.Bundle;
 import android.widget.Button;
-import android.widget.GridLayout;
-import android.widget.GridView;
 import android.widget.RelativeLayout;
-import android.widget.TableLayout;
 import android.widget.TableRow;
-import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class GamePage extends AppCompatActivity {
 private TableRow tableRow;
@@ -27,6 +20,7 @@ private RelativeLayout relativeLayout;
 
         for (i=0;i<100;i++) {
             relativeLayout.addView(new Button(this));
+
         }
 
 
