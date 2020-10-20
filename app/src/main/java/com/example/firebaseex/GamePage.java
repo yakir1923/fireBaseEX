50a8b1bfc64477309a3fbac958b09fba49646879
yakir <yakir1923@gmail.com>
2020-10-20 17:39:21 +0300

    20/10/20 לוח משחק מעודכן

--- app/src/main/java/com/example/firebaseex/GamePage.java
+++ app/src/main/java/com/example/firebaseex/GamePage.java
@@ -1,6 +1,9 @@
 package com.example.firebaseex;
 
+import android.content.SharedPreferences;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +15,7 @@ import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
@@ -22,18 +26,42 @@ private Button button;
 private int i,j;
 private TableLayout tableLayout;
 private TableRow tableRow;
+private TextView playerPoints;
+private TextView playerName;
+private TextView opponentName;
+private TextView opponentPoints;
+private int playerCurrentPoints;
+private int opponentCurrentPoints;
+private SharedPreferences.Editor editor;
+private SharedPreferences userDitale;
+private Drawable drawable;
+private int idNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_page);
         tableLayout=findViewById(R.id.tableLayout);
+        playerPoints=findViewById(R.id.player_points);
+        playerName=findViewById(R.id.player_name);
+        opponentName=findViewById(R.id.opponent_name);
+        opponentPoints=findViewById(R.id.opponent_points);
+        playerCurrentPoints=0;
+        opponentCurrentPoints=0;
+        playerPoints.setText("0");
+        opponentPoints.setText("0");
+        userDitale=getSharedPreferences("login",MODE_PRIVATE);
+        playerName.setText(userDitale.getString("name",null));
+        idNum=0;
+
         for (i=0;i<10;i++) {
             tableRow=new TableRow(this);
             tableLayout.addView(tableRow);
             for (j=1;j<11;j++){
                   button=new Button(this);
-                  button.setId(i);
+                  button.setId(idNum);
+                  idNum++;
+                  button.setBackground(getDrawable(R.drawable.my_button));
                   button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
