9fd1c9b15ef153ac68e76c143a1bb901a121d11f
shanimoz <65355962+shanimoz@users.noreply.github.com>
2020-10-20 23:28:28 +0300

    עיצוב לוח משחק הוספת מוזיקה

--- app/src/main/java/com/example/firebaseex/settingsPage.java
+++ app/src/main/java/com/example/firebaseex/settingsPage.java
@@ -1,33 +1,60 @@
 package com.example.firebaseex;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class settingsPage extends AppCompatActivity {
     private Button backHomeButton;
     private Intent goTOHome;
     public Bundle bundle;
 
+    private Switch music_switch;
+
+    private MediaPlayer music;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_page);
 
+        music_switch= (Switch) findViewById(R.id.music_switch);
         backHomeButton=findViewById(R.id.backButton);
         goTOHome=new Intent(this,User_profile.class);
         backHomeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(settingsPage.this, "back",Toast.LENGTH_LONG).show();
                 startActivity(goTOHome);
             }
         });
+
+        music=MediaPlayer.create(settingsPage.this,R.raw.background_music);
+        music_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener(){
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    playIt();
+                }
+                else {
+                    onPause();
+                }
+            }
+            });
+
     }
-    //דך הגדרות
+    public void playIt(){
+
+            music.start();
+
+    }
+
+    protected void onPause(){
+        super.onPause();
+        music.release();
+    }
 }
