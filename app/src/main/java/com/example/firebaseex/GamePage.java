e3aba5d01101f7e70e61be88030c40d0f6a856fa
shanimoz <65355962+shanimoz@users.noreply.github.com>
2020-10-24 13:03:59 +0300

    העלתי אותיות צריך לחבר בין אות לתמונה

--- app/src/main/java/com/example/firebaseex/GamePage.java
+++ app/src/main/java/com/example/firebaseex/GamePage.java
@@ -13,6 +13,7 @@ import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -68,28 +69,28 @@ private Intent showActivity;
         nextTurn.setText("סיום");
         buttonList=new ArrayList<Button>();
         letterArrayList=new ArrayList<Letter>();
-        letterArrayList.add(new Letter("א",10,"---"));
-        letterArrayList.add(new Letter("ב",10,"---"));
-        letterArrayList.add(new Letter("ג",20,"---"));
-        letterArrayList.add(new Letter("ד",30,"---"));
-        letterArrayList.add(new Letter("ה",10,"---"));
-        letterArrayList.add(new Letter("ו",5,"---"));
-        letterArrayList.add(new Letter("ז",80,"---"));
-        letterArrayList.add(new Letter("ח",30,"---"));
-        letterArrayList.add(new Letter("ט",40,"---"));
-        letterArrayList.add(new Letter("י",30,"---"));
-        letterArrayList.add(new Letter("כ",70,"---"));
-        letterArrayList.add(new Letter("ל",40,"---"));
-        letterArrayList.add(new Letter("מ",60,"---"));
-        letterArrayList.add(new Letter("נ",30,"---"));
-        letterArrayList.add(new Letter("ס",50,"---"));
-        letterArrayList.add(new Letter("ע",80,"---"));
-        letterArrayList.add(new Letter("פ",90,"---"));
-        letterArrayList.add(new Letter("צ",100,"---"));
-        letterArrayList.add(new Letter("ק",60,"---"));
-        letterArrayList.add(new Letter("ר",40,"---"));
-        letterArrayList.add(new Letter("ש",50,"---"));
-        letterArrayList.add(new Letter("ת",40,"---"));
+        letterArrayList.add(new Letter("א",10,"alef"));
+        letterArrayList.add(new Letter("ב",10,"beit"));
+        letterArrayList.add(new Letter("ג",20,"gimel"));
+        letterArrayList.add(new Letter("ד",30,"daled"));
+        letterArrayList.add(new Letter("ה",10,"hei"));
+        letterArrayList.add(new Letter("ו",5,"vav"));
+        letterArrayList.add(new Letter("ז",80,"zain"));
+        letterArrayList.add(new Letter("ח",30,"chet"));
+        letterArrayList.add(new Letter("ט",40,"tet"));
+        letterArrayList.add(new Letter("י",30,"yud"));
+        letterArrayList.add(new Letter("כ",70,"chaf"));
+        letterArrayList.add(new Letter("ל",40,"lamed"));
+        letterArrayList.add(new Letter("מ",60,"mem"));
+        letterArrayList.add(new Letter("נ",30,"non"));
+        letterArrayList.add(new Letter("ס",50,"samech"));
+        letterArrayList.add(new Letter("ע",80,"aein"));
+        letterArrayList.add(new Letter("פ",90,"pey"));
+        letterArrayList.add(new Letter("צ",100,"tzadik"));
+        letterArrayList.add(new Letter("ק",60,"kof"));
+        letterArrayList.add(new Letter("ר",40,"resha"));
+        letterArrayList.add(new Letter("ש",50,"shin"));
+        letterArrayList.add(new Letter("ת",40,"taf"));
 
 
         for (i=0;i<10;i++) {
@@ -124,11 +125,19 @@ private Intent showActivity;
         for (i=0;i<10;i++){
             Random random=new Random();
             int rndNum=random.nextInt(22);
+
             playerHand.addView(button=new MyButton(this,0,0));
+
             button.setBackground(getDrawable(R.drawable.my_button));
             button.setText(letterArrayList.get(rndNum).getLett());
             button.setLetter(letterArrayList.get(rndNum).getLett());
-            button.setBackground(getDrawable(R.drawable.active_button_color));
+
+
+            String str="res/drawable/" +letterArrayList.get(rndNum).getIcon() + ".png";
+            File file = new File(str);
+            button.setBackground(getDrawable(R.drawable.);
+
+
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
